f2a9b93d2c285730c5ebe7c9e43b8b824c282dc2
DaikichiTanaka <t.daiki0929@gmail.com>
2021-03-03 16:32:09 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英・数字の両方を含めて設定して下さい'} 
   with_options presence:true do
-    validates :user_check_id
+    validates :user_check_id, numericality:{other_than: 0, message: "can't be blank"} 
     validates :nickname
     validates :birthday
     with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用して下さい'} do
@@ -18,9 +19,6 @@ class User < ApplicationRecord
     end
     validates :telephone, format:{with: /\A\d{11}\z/, message: "is invalid. 11桁以内の半角数字で入力してください"}
   end
-    
-  validates :user_check_id, numericality:{other_than: 0, message: "can't be blank"} 
-
  
   # Associations
   extend ActiveHash::Associations::ActiveRecordExtensions
