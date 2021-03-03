302399c5021a6d4c5f40d6021262c83d824f4384
DaikichiTanaka <t.daiki0929@gmail.com>
2021-03-03 14:34:16 +0900

    ユーザー詳細・編集ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user =User.find(current_user.id)
   end
 
   def update
+    @user =User.find(current_user.id)
+    if @user.update(user_params)
+        redirect_to user_path(current_user.id)
+      else
+        render :edit 
+      end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :telephone, :office, :career, :introduction)
   end
 end
