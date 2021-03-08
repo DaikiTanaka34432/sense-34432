2f728e1a90bd41ef667c6dd6d1131c513e2e138f
DaikichiTanaka <t.daiki0929@gmail.com>
2021-03-08 14:29:33 +0900

    トークン化の処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_f67181a0fa3d0d0ec83a636d");
   const form= document.getElementById("charge-form");
   form.addEventListener("submit", (e)=> {
     e.preventDefault(); // Railsにおけるフォーム送信処理をキャンセル
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);  
+  
+    const card ={
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) =>{
+      if  (status ==200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
