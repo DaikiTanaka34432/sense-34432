3caeb7a8dd258f49e584e19265c7a2d67606e0ce
DaikichiTanaka <t.daiki0929@gmail.com>
2021-03-10 12:52:15 +0900

    商品検索機能の実装

--- app/models/office.rb
+++ app/models/office.rb
@@ -4,7 +4,7 @@ class Office < ActiveHash::Base
     {id:1, name: 'よしもとクリエイティブ・エージェンシー'},
     {id:2, name: 'ワタナベエンターテインメント'},
     {id:3, name: '太田プロダクション'},
-    {id:4, name: '人力舎'},
+    {id:4, name: 'プロダクション人力舎'},
     {id:5, name: 'マセキ芸能社'},
     {id:6, name: '松竹芸能'},
     {id:7, name: 'サンミュージック'},
@@ -14,8 +14,10 @@ class Office < ActiveHash::Base
     {id:11, name: 'タイタン'},
     {id:12, name: 'ホリプロコム'},
     {id:13, name: 'グレープカンパニー'},
-    {id:14, name: 'スターダストプロモーション'},
-    {id:15, name: 'その他、フリー'},
+    {id:14, name: 'ケイダッシュステージ'},
+    {id:15, name: 'SMA'},
+    {id:16, name: 'ASH&Dコーポレーション'},
+    {id:17, name: 'その他、フリー'},
   ]
 
   include ActiveHash::Associations
